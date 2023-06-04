cc308a81d5d3b7e6cae8b119d1daa615d763092c
Jorge de jesus cardoso <133919477+jorgejesuscardoso@users.noreply.github.com>
2023-06-03 22:09:23 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let formulario = document.getElementById("formLogin");
+  
+    formulario.addEventListener("submit", function(event) {
+      event.preventDefault();
+  
+      let login = formulario.elements["login"].value;
+      let senha = formulario.elements["senha"].value;
+  
+      let dados = {
+        login: login,
+        senha: senha
+      };
+  
+      if (dados.login === "Bushido" && dados.senha === "123" || dados.login === "Kauã" && dados.senha === "123"  ) {
+        boasVindas(dados.login);
+        window.location.href = "index.html"
+      } else {
+        mensagemErro();
+      }
+    });
+  
+    function boasVindas(login) {
+      let mensagem = 'Seja bem-vindo ' + login + '. Aproveite o site!';
+      alert(mensagem);
+    }
+  
+    function mensagemErro() {
+      alert("Login e/ou senha incorretos. Tente novamente ou registre-se!.");
+    }
+  })
