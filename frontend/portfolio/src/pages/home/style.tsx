ce657485a6df3d56c4b8f376366658c258feaf04
Jorge De Jesus Cardoso <tst.jorgecardoso@gmail.com>
2024-04-08 18:22:15 -0300

    refat iframe

--- frontend/portfolio/src/pages/home/style.tsx
+++ frontend/portfolio/src/pages/home/style.tsx
@@ -4,19 +4,19 @@ export const MainContainer = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
   width: 100%;
   height: 90vh;
-  background-color: var(--frame-bg);
+  margin-top: 11vh;
 `;
 export const IFrameYt = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 100%;
+  height: 70vh;
+  background-color: var(--frame-bg);
   iframe {
     width: 70%;
-    height: 100%;
+    height: 90%;
   }
 `;
